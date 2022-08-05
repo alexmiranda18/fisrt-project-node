9a7561262b574913335734f6b7d584b3136a536e
alexmiranda18 <alexsousamiranda@hotmail.com>
2022-08-04 21:46:53 -0300

    first-project-node

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const uuid = require('uuid')
+const app = express()
+const port = 3000
+app.use(express.json())
+
+const demand = []
+const checkUserId = (request, response, next) => {
+    const { id } = request.params
+
+    const index = demand.findIndex(index => index.id === id)
+
+    if (index < 0) {
+        return response.status(404).json({ error: "Order not found." })
+    }
+
+    userId = id
+    userIndex = index
+
+    next()
+}
+
+const MethodUrl = (request, response, next) => {
+    const method = request.method
+    const url = request.path
+    console.log (`Method:${ method} URL: ${url}`)
+    next()
+}
+
+app.get('/command', MethodUrl,(request, response) => {
+    return response.json(demand)
+})
+
+app.post('/command',MethodUrl, (request, response) => {
+    const { order, clientName, price, status } = request.body
+
+    const orderClient = { id: uuid.v4(), order, clientName, price, status }
+
+    demand.push(orderClient)
+    return response.status(201).json(orderClient)
+})
+app.put('/command/:id', checkUserId,MethodUrl, (request, response) => {
+    const id = request.userId
+    const { order, clientName, price, status } = request.body
+    const updateOrder = { id, order, clientName, price, status }
+
+    demand[userIndex] = updateOrder
+
+    return response.json(updateOrder)
+})
+app.delete('/command/:id', checkUserId, MethodUrl, (request, response) => {
+
+    demand.splice(userIndex, 1)
+    return response.status(204).json()
+})
+
+
+app.get('/command/:id', checkUserId, MethodUrl, (request, response) => {
+    const index = userIndex
+
+    return response.json(demand[index])
+})
+
+app.patch('/command/:id', checkUserId, MethodUrl,(request, response) => {
+    const index = userIndex
+
+    demand[index].status = "Pronto"
+
+    return response.json(demand[index])
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`server started on port ${port}`)
+})
