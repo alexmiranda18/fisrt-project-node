b8e256484daed86305c3c7cc55b258703f1087d1
alexmiranda18 <alexsousamiranda@hotmail.com>
2024-08-19 15:00:35 -0300

    feat: config vercel deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const uuid = require('uuid')
 const app = express()
-const port = 3001
+const port = process.env.PORT || 3001
 const cors = require('cors')
 app.use(express.json())
 app.use(cors())
